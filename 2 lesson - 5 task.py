b30513bbe0dd0a143c6b154ca3d8993497a171a7
KV1talya <79435205+KV1talya@users.noreply.github.com>
2021-03-20 22:26:17 +0900

    Вроде сделал...

--- 2 lesson - 5 task.py
+++ 2 lesson - 5 task.py
@@ -1,5 +1,7 @@
 price = [57.8, 46.51, 97, 33, 1, 18.8, 99.9, 199.9, 88, 77, 88.8, 33.9, 55.7, 0.99]
+price_down = []
 text_out = ""
+text_out_down = ""
 
 for number in price:
     if type(number) == int:
@@ -12,3 +14,20 @@ for number in price:
         text_out += f"{rub} руб {kop} коп, "
 
 print(text_out)
+
+price.sort()
+print(price)
+
+for price_d in price:
+    price_down.append(price_d)
+
+price_down.sort(reverse=True)
+print(price_down)
+
+n = 0
+for five in price:
+    text_out_down += f"{five} "
+    n += 1
+    if n == 5:
+        break
+print(text_out_down)
