1e5a4dc2716aa686ee514d870cf62576ed94e6ce
KV1talya <79435205+KV1talya@users.noreply.github.com>
2021-03-31 20:39:54 +0900

    import utils
    75.6373, 01.04.2021
    88.7452, 01.04.2021
    75.6373, 01.04.2021
    88.7452, 01.04.2021
    None
    None

---
+++ 4lesson_4task.py
@@ -0,0 +1,9 @@
+import utils
+
+print(utils.currency_rates("USD"))
+print(utils.currency_rates("EUR"))
+print(utils.currency_rates("UsD"))
+print(utils.currency_rates("Eur"))
+
+print(utils.currency_rates("XXX"))
+print(utils.currency_rates("123"))
