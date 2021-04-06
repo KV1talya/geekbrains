abdf1a439f3209d46c800a05fed3e9e3202af2a7
KV1talya <79435205+KV1talya@users.noreply.github.com>
2021-04-06 21:18:49 +0900

    Add files via upload

---
+++ 5lesson_1-2task.py
@@ -0,0 +1,18 @@
+def nums_generator(max_num):
+    for num in range(1, max_num + 1):
+        if num % 2:
+            yield num
+
+
+print(type(nums_generator(15)))
+for _ in nums_generator(15):
+    print(_)
+
+
+def nums_generator2(max_num):
+    return (x for x in range(1, max_num + 1, 2))
+
+
+print(type(nums_generator2(15)))
+for _ in nums_generator2(15):
+    print(_)
