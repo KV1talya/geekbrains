4bdf0b52bc9e273fff24bf42238ab4080c9c5b7a
KV1talya <79435205+KV1talya@users.noreply.github.com>
2021-04-14 22:30:52 +0900

    1task

---
+++ 8lesson_1task.py
@@ -0,0 +1,17 @@
+import re
+
+
+def email_parse(email: str) -> dict:
+    email_return = {}
+    if re.match(r'^[0-9A-Za-z]+@[0-9A-Za-z]+\.[A-Za-z]+', email):
+        username, domain = email.split("@")
+        email_return.update({"username": username, "domain": domain})
+        return email_return
+    else:
+        raise ValueError(email)
+
+
+if __name__ == '__main__':
+    print(email_parse("someone@geekbrains.ru"))
+    print(email_parse("someone@geekbrainsru"))
+    # print(email_parse("someone%geekbrains.ru"))
