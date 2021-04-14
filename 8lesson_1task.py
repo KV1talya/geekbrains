e6ae3cf5178c171e4333442a7e62a2ea59f22584
KV1talya <79435205+KV1talya@users.noreply.github.com>
2021-04-14 22:33:28 +0900

    re

--- 8lesson_1task.py
+++ 8lesson_1task.py
@@ -3,7 +3,7 @@ import re
 
 def email_parse(email: str) -> dict:
     email_return = {}
-    if re.match(r'^[0-9A-Za-z]+@[0-9A-Za-z]+\.[A-Za-z]+', email):
+    if re.match(r'^[0-9A-Za-z]+@[0-9A-Za-z]+\.[A-Za-z]\D+', email):
         username, domain = email.split("@")
         email_return.update({"username": username, "domain": domain})
         return email_return
